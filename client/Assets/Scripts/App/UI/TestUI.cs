5d029729e78c3726598b947f15b5af7902c18449
halneko <otsuichi5121@gmail.com>
2020-10-17 17:38:48 +0900

    add テスト用のUIスクリプトを追加。

---
+++ client/Assets/Scripts/App/UI/TestUI.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace RandB
+{
+    public class TestUI : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        //---------------------------------------------
+        // MonoBehaviour
+        //---------------------------------------------
+        void Start()
+        {
+            // ゆるやかな拡大：
+            // InOutCirc、InOutQuint
+            // OutQuint、OutQuart
+            // InOutCubic、OutCubic
+            // OutQuad
+            var parent = animationText.gameObject.transform.parent.gameObject;
+            Debug.Log("--------------------");
+            Debug.Log(parent);
+            parent.transform.DOLocalMoveX(100.0f, 2.0f).SetRelative(true).SetEase(Ease.InOutCirc);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        }
+
+        //---------------------------------------------
+        // Method
+        //---------------------------------------------
+
+        //---------------------------------------------
+        // Const/Enum
+        //---------------------------------------------
+
+        //---------------------------------------------
+        // Property
+        //---------------------------------------------
+
+        //---------------------------------------------
+        // Field
+        //---------------------------------------------
+        [SerializeField]
+        private TMP_Text animationText = null;
+
+        private Text hoge = null;
+    }
+}
