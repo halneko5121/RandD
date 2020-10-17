179deb6c89120d2eeab62a8fd4d66e8a9fa296e4
halneko <otsuichi5121@gmail.com>
2020-10-17 18:13:42 +0900

    add ヒットストップの検証

--- client/Assets/Scripts/App/UI/TestUI.cs
+++ client/Assets/Scripts/App/UI/TestUI.cs
@@ -23,17 +23,45 @@ namespace RandB
             var parent = animationText.gameObject.transform.parent.gameObject;
             Debug.Log("--------------------");
             Debug.Log(parent);
-            parent.transform.DOLocalMoveX(100.0f, 2.0f).SetRelative(true).SetEase(Ease.InOutCirc);
+            parent.transform.DOLocalMoveX(500.0f, 3.0f).SetRelative(true).SetEase(Ease.InOutCirc);
         }
 
         // Update is called once per frame
         void Update()
         {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                SlowDown();
+            }
+
+            //　スローダウンフラグがtrueの時は時間計測
+            if (isSlowDown)
+            {
+                elapsedTime += Time.unscaledDeltaTime;
+                if (elapsedTime >= slowTime)
+                {
+                    SetNormalTime();
+                }
+            }
         }
 
         //---------------------------------------------
         // Method
         //---------------------------------------------
+
+        //　時間を遅らせる処理
+        public void SlowDown()
+        {
+            elapsedTime = 0f;
+            Time.timeScale = timeScale;
+            isSlowDown = true;
+        }
+        //　時間を元に戻す処理
+        public void SetNormalTime()
+        {
+            Time.timeScale = 1f;
+            isSlowDown = false;
+        }
 
         //---------------------------------------------
         // Const/Enum
@@ -49,6 +77,13 @@ namespace RandB
         [SerializeField]
         private TMP_Text animationText = null;
 
-        private Text hoge = null;
+        //　Time.timeScaleに設定する値
+        private float timeScale = 0.1f;
+        //　時間を遅くしている時間
+        private float slowTime = 1f;
+        //　経過時間
+        private float elapsedTime = 0f;
+        //　時間を遅くしているかどうか
+        private bool isSlowDown = false;
     }
 }
