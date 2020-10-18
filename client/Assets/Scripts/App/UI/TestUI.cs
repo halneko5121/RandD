60689b1e910993bf725b6b6fe965b10bef3fa937
halneko <otsuichi5121@gmail.com>
2020-10-18 17:03:41 +0900

    update テスト用UIを更新。

--- client/Assets/Scripts/App/UI/TestUI.cs
+++ client/Assets/Scripts/App/UI/TestUI.cs
@@ -4,29 +4,57 @@ using TMPro;
 using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Diagnostics;
 
 namespace RandB
 {
     public class TestUI : MonoBehaviour
     {
-        // Start is called before the first frame update
         //---------------------------------------------
         // MonoBehaviour
         //---------------------------------------------
         void Start()
         {
+            // 文字数カウント
+            templeteText.gameObject.SetActive(true);
+            var count = templeteText.text.Length;
+            char[] textString = templeteText.text.ToCharArray();
+            UnityEngine.Debug.Log("----------------");
+            for (int i = 0; i < count; i++)
+            {
+                var text = Object.Instantiate(templeteText);
+                text.gameObject.transform.SetParent(this.transform);
+                text.gameObject.transform.localPosition = Vector3.zero;
+                text.text = textString[i].ToString();
+                stringList.Add(text);
+                UnityEngine.Debug.Log(text.text);
+            }
+            templeteText.gameObject.SetActive(false);
+
             // ゆるやかな拡大：
             // InOutCirc、InOutQuint
             // OutQuint、OutQuart
             // InOutCubic、OutCubic
             // OutQuad
+            float targetX = 500.0f;
+            float stringSize = 150.0f;
+            for (int i = 0; i < stringList.Count; i++)
+            {
+                var str = stringList[i];
+                var sequence = DOTween.Sequence()
+                    .Append(str.transform.DOLocalMoveX(-targetX + (stringSize * i), 0.5f).SetDelay(i * 0.5f).SetEase(Ease.InOutCirc))
+                    .Append(str.transform.DORotate(new Vector3(0.0f, 0.0f, 360.0f), 0.5f).SetRelative(true).SetEase(Ease.InOutCirc))
+                    ;
+            }
+
+            /*
             var parent = animationText.gameObject.transform.parent.gameObject;
             Debug.Log("--------------------");
             Debug.Log(parent);
             parent.transform.DOLocalMoveX(500.0f, 3.0f).SetRelative(true).SetEase(Ease.InOutCirc);
+            */
         }
 
-        // Update is called once per frame
         void Update()
         {
             if (Input.GetKeyDown(KeyCode.Space))
@@ -76,6 +104,11 @@ namespace RandB
         //---------------------------------------------
         [SerializeField]
         private TMP_Text animationText = null;
+
+        [SerializeField]
+        private TMP_Text templeteText = null;
+
+        List<TMP_Text> stringList = new List<TMP_Text>();
 
         //　Time.timeScaleに設定する値
         private float timeScale = 0.1f;
